f01843e24bb3e7e09d269cf8473219aa210d1017
DUOLabs333 <dvdugo333@gmail.com>
2023-09-16 01:57:03 -0400

    Finished everything except function pointer, though probably have to start over due to unresolved bug in xmltodict

--- parse/parse.py
+++ parse/parse.py
@@ -5,15 +5,21 @@ sys.path.append("_vendor")
 import xmltodict
 
 vk=xmltodict.parse(open("../Vulkan-Headers/registry/vk.xml").read())["registry"]
-handles={}
-structs={}
 aliases={}
+
+handles=set()
+structs={}
+commands={}
+funcpointers={}
+primitive_types=set()
+
 for item in vk["types"]["type"]:
     type=item.get("@category","")
+        
     if type=="struct":
         if "@alias" in item:
             if item["@alias"] not in structs:
-                aliases[item["@alias"]]=item["@name"]
+                aliases[item["@alias"]]=item["@name"] #For aliases that are defined before their parrent is defined (only one case though)
                 continue
             members=structs[item["@alias"]]
         else:
@@ -29,5 +35,34 @@ for item in vk["types"]["type"]:
         if item["@name"] in aliases:
             structs[aliases[item["@name"]]]=members
             del aliases[item["@name"]]
-
-print(aliases)
+    elif type=="handle":
+        if "name" in item:
+            handles.add(item["name"])
+        elif "@alias" in item:
+            handles.add(item["@alias"])
+    elif type=="funcpointer":
+        print(item)
+for item in vk["commands"]["command"]:
+    command={}
+    if "@alias" in item:
+        name=item["@alias"]
+        if name not in commands:
+            aliases[name]=item["@name"] #For aliases that are defined before their parrent is defined
+            continue
+        command=commands[name]
+    else:
+        name=item["proto"]["name"]
+        item["@successcodes"]=item.get("@successcodes","")
+        item["@errorcodes"]=item.get("@errorcodes","")
+        if ("VK_TIMEOUT" in item["@successcodes"].split(",")) or ("VK_TIMEOUT" in (item["@errorcodes"].split(","))):
+            command["sync"]=True
+        else:
+            command["sync"]=False
+        command["return"]=item["proto"]["type"]
+        command["arguments"]=item["param"]
+    
+    commands[name]=command
+    
+    if name in aliases:
+        commands[aliases[name]]=command
+        del aliases[name]
