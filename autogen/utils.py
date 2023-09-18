74a5fc720225036cd4d71cf301bb499e28c30e72
DUOLabs333 <dvdugo333@gmail.com>
2023-09-18 16:37:09 -0400

    Finish general deserialize function

--- autogen/utils.py
+++ autogen/utils.py
@@ -17,12 +17,12 @@ json [&](){{
     json result=json({{}});
     
     if ({name}==NULL){{
-        result={serialize(name,"NULL",num_indirection,length,client)};
+        result={serialize_null(name,client)};
     }} 
-    else if ({(len(length)>0 and length[-1]!='') and not(type=="char" and num_indirection==1)}){{
+    else if ({(len(length)>0 and (length[-1] not in ['',"null-terminated"])) and not(type=="char" and num_indirection==1)}){{
         result["members"]={{}};
         for(int i=0; i < {length[-1]}; i++){{
-            result["members"][std::to_string(i)].push_back({serialize(name+"[i]",num_indirection-1,length[:-1],client});
+            result["members"][std::to_string(i)].push_back({serialize(name+"[i]",num_indirection-1,length[:-1],client)});
         }}
     }}
     else if ({num_indirection>0}){{
@@ -34,6 +34,31 @@ json [&](){{
     return result;
 }}();
 """
+    return result
+
+def deserialize(name,type,num_indirection,length,client=True):
+    result=f"""
+auto [&](){{
+    if ({name}["null"]){{
+        return NULL;
+    }}
+    else if ({(len(length)>0 and (length[-1] not in ['',"null-terminated"])) and not(type=="char" and num_indirection==1)}){{
+        auto members=malloc({length[-1]}*{type+"*"*(num_indirection-1)});
+        for (int i=0; i < {length[-1]}; i++){{
+            members[i]={deserialize({name+'["members"][i]'},type,num_indirection-1,length[:-1],client)}
+        }}
+        return members;
+    }}
+    else if ({num_indirection>0}){{
+        auto pointer={deserialize(name,type,num_indirection-1,length,client)};
+        return &pointer;
+    }}
+    else {{
+        return deserialize_{type}(name,{client});
+    }}
+}}();
+    """
+    
     return result
 def write(line,header=False):
     if not header:
