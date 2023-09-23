cb01cb2073b6acfc3f7604f33ff27bdca1b1c023
DUOLabs333 <dvdugo333@gmail.com>
2023-09-22 22:38:18 -0400

    Finish first version of the server implementation

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,20 +1,30 @@
-/*
-When you recieve a new connection, make a new thread to handle it. Keep thread id. When session is later disconnected, exit thread
-sendtoConn/readfromConn looks up the conn in thread_to_conn (pthread_self and pthread_create)
-
-Keep reading until new line
-*/
 #include <pthread.h>
 
+auto service = std::make_shared<CppServer::Asio::Service>();
 
-void thread_func(){
+void *thread_func(void *arg){
+    //Will only be called by the server
+    
+    while(thread_to_conn.find(pthread_self())==0){ // Prevent race
+    }
+    
     while(true){
-        std::string data_string=readfromConn();
-        json data=parseJSON(data_string);
+        if(!isConnConnected()){
+            break;
+        }
+        json data=readfromConn();
         
         std::string type=data["type"];
-        if type==
-std::<pthread_t, std::shared_ptr< TCPSession >> thread_to_conn;
+        if (type=="sync_init"){
+            handle_sync_init(data);
+        }
+        else (if type=="Command"){
+            handle_Command(data);
+        }
+    }
+}
+std::map<pthread_t, std::shared_ptr< TCPSession >> thread_to_conn;
+std::map<std::shared_ptr< TCPSession >, stringstream> conn_to_stream;
 
 class StreamServer : public CppServer::Asio::TCPServer
 {
@@ -25,3 +35,62 @@ public:
 protected:
     void onConnected(std::shared_ptr< TCPSession > &session){
             pthread_t thread_id;
+            pthread_create(&thread_id,NULL,thread_func,NULL);
+            thread_to_conn[thread_id]=session;
+            auto ss=stringstream()
+            conn_to_stream[session]=&ss;
+    }
+};
+
+class StreamClient : public CppServer::Asio::TCPClient
+{
+public:
+    ChatClient(std::shared_ptr<CppServer::Asio::Service> service, const std::string& address, int port)
+        : CppServer::Asio::TCPClient(service, address, port)
+    {
+    }
+};
+
+bool isConnConnected(){
+    //Will only be called by the server
+    return thread_to_conn[pthread_self()]->IsConnected();
+}
+
+json readfromConn(){
+    pthread_t thread_id=pthread_self();
+    
+    auto conn=thread_to_conn[thread_id];
+    auto stream=conn_to_stream[conn];
+            
+    
+    while(true){
+        string word;
+        getline(*stream,word);
+        if (!(*stream).eof()){ //Must have gotten a \n
+           break;
+        }else{
+            *stream << conn->Recieve(64);
+        }
+    }
+    return json::parse(word);
+}
+
+void writetoConn(json data){
+
+    #ifdef CLIENT
+        if(thread_to_conn.count(thread_id)==0){
+            thread_to_conn[thread_id]=std::make_shared<StreamClient>(service, address, port);
+            thread_to_conn[thread_id]->Start();
+            service->Start(); //Make sure it's on
+        }
+    #endif
+        
+    thread_to_conn[thread_id]->Send(data.dump()+"\n");
+}
+    
+
+auto startServer(){
+    server = std::make_shared<StreamServer>(service, port);
+    server->Start();
+    return server; 
+}        
